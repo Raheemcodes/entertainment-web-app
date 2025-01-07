e1870d5a90839c3e2c920cd8deee61da495254ed
Raheemcodes <raheemscorporatemail@gmail.com>
2025-01-07 09:37:52 +0100

    Create Login & Sign pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+import { JSX } from 'react';
+import classes from './login.module.scss';
+
+const LoginPage = (): JSX.Element => {
+  return <h1>Login</h1>;
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+import { JSX } from 'react';
+import classes from './signup.module.scss';
+
+const SignupPage = (): JSX.Element => {
+  return <h1>Signup</h1>;
+};
+
+export default SignupPage;
